42388e9714329bb16c53acf1b1bd183795df1cd4
Angelo Pumar <angelopumar987@gmail.com>
2024-10-05 00:10:00 +0800

    Update Limit To 5 per 30 sec

--- middleware/rateLimitMiddleware.js
+++ middleware/rateLimitMiddleware.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const rateLimitMiddleware = rateLimit({
-    windowMs: 15 * 60 * 1000, 
-    max: 100, 
+    windowMs: 30 * 1000, 
+    max: 5, 
     message: { error: "Too many requests, please try again later." },
 });
 
